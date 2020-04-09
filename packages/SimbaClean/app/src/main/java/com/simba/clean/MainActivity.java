5f5f5a90faa7b7b652cb52dfa96e8af9e6387576
lijian <lijian@simbalink.cn>
2020-04-09 18:24:18 +0800

    实现view-1

--- packages/SimbaClean/app/src/main/java/com/simba/clean/MainActivity.java
+++ packages/SimbaClean/app/src/main/java/com/simba/clean/MainActivity.java
@@ -3,8 +3,10 @@ package com.simba.clean;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -20,19 +22,33 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         iv_loading_memory = findViewById(R.id.iv_loading_memory);
         startMemoryLoadingAnim();
+
     }
 
 
-    public void startMemoryLoadingAnim(){
+    public void startMemoryLoadingAnim() {
         animator = ObjectAnimator.ofFloat(iv_loading_memory, "rotation", 0f, 360f);//旋转360度
+        animator.setInterpolator(new LinearInterpolator());
         animator.setRepeatCount(-1);//无限循环
         animator.setDuration(2000);//设置持续时间
         animator.start();//动画开始
     }
 
-    public void setFullScreen(){
+    public void setFullScreen() {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
     }
+
+        @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
 }
