275ea91e1f844acb3bef91de33b51ceafd6f2f95
chenjianbo <nj.chen@qq.com>
2020-04-13 17:25:47 +0800

    切图更新

--- packages/ViolationEnquiry/app/src/main/java/com/simba/violationenquiry/ui/CarInfoDetailFragment.java
+++ packages/ViolationEnquiry/app/src/main/java/com/simba/violationenquiry/ui/CarInfoDetailFragment.java
@@ -146,10 +146,6 @@ public class CarInfoDetailFragment extends BaseLazyLoadFragment implements View.
 
         tvErrorPlateNo.setText(carInfo.getPlateno());
 
-
-        if (index == 0) {
-            loadData(true, false);
-        }
     }
 
     @Override
