43a1df370174b4ee8853ded5e7f0dcb01dd10338
lijian <lijian@simbalink.cn>
2020-04-14 09:09:39 +0800

    CLEAN   -->  提交gradle  配置

--- packages/SimbaClean/app/src/main/java/com/simba/clean/MainActivity.java
+++ packages/SimbaClean/app/src/main/java/com/simba/clean/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
