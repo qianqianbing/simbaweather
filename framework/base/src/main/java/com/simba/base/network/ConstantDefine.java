a9b351c867418893b173879c50022a3d2f85560d
luojunjie <luojunjie@simbalink.cn>
2020-04-17 17:13:35 +0800

    会员中心接口地址

---
+++ framework/base/src/main/java/com/simba/base/network/ConstantDefine.java
@@ -0,0 +1,18 @@
+/**
+ * 作    者：luojunjie
+ * 版    本：1.0
+ * 创建时间: 2020/4/17
+ * 描    述：
+ */
+package com.simba.base.network;
+
+public class ConstantDefine {
+
+    //********************************************* 会员中心 ******************************************
+    public static final String ACTION = "action";
+    public static final String CALLBACKURL = "callbackurl";
+    public static final String DEVICEID = "deviceid";
+    public static final String WeChatURL = "http://simbaui.simbalink.cn";
+    public static final int QRTYPE_ACTIVATION = 0; //二维码类型 0：激活
+    public static final int QRTYPE_LOGIN = 1; //二维码类型 1：登陆
+}
