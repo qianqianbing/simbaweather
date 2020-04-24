df411b8c1f8d10c9f8ad6f6918a2134dfd436300
luojunjie <luojunjie@simbalink.cn>
2020-04-24 16:18:32 +0800

    增加包名统一定义

---
+++ framework/base/src/main/java/com/simba/base/define/ModelDefine.java
@@ -0,0 +1,15 @@
+/**
+ * 作    者：luojunjie
+ * 版    本：1.0
+ * 创建时间: 2020/4/17
+ * 描    述：
+ */
+package com.simba.base.define;
+
+public class ModelDefine {
+
+    //********************************************* 会员中心 包名 activity******************************************
+    public static final String MEMBERCENTER_PACKAGENAME = "com.simba.membercenter";
+    public static final String MEMBERCENTER_MAIN_ACTIVITY = "com.simba.membercenter.view.MainActivity";
+
+}
